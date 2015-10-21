967a0b6ff135e732a126e5dd674a805b1785f67d
Rich Lucente <rlucente@Richs-MacBook-Pro.local>
2015-10-20 21:46:26 -0400

    maximize size of window

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # launch the Xvnc server which will launch openbox and your app
-vncserver :1 -name 'Desktop Name' -geometry 800x600 -depth 24
+vncserver :1 -name 'Desktop Name' -geometry 1440x726 -depth 24
 
 # expose the Xvnc server via guacamole
 sudo systemctl start guacd
